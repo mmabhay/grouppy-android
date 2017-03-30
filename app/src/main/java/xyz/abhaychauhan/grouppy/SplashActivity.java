d3c1d283b67d085cc02cad8215caf3fe4c31f6c0
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2017-03-30 13:27:15 +0530

    feat: Splash Screen

---
+++ app/src/main/java/xyz/abhaychauhan/grouppy/SplashActivity.java
@@ -0,0 +1,47 @@
+package xyz.abhaychauhan.grouppy;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by abhay on 30/03/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.app_title_tv)
+    TextView splashTitleTv;
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ButterKnife.bind(this);
+        Typeface typeface = Typeface.createFromAsset(getApplicationContext().getAssets(),
+                "fonts/Chewy.ttf");
+        splashTitleTv.setTypeface(typeface);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
